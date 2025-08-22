a99b5bffb386fc282c9a94f37fdc81a7397d015f
vlvsv <vvs_2015@icloud.com>
2025-08-22 21:42:29 +0300

    Initial Commit

---
+++ GyverSettings/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GyverSettings
+//
+//  Created by Vlad V on 22.08.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GyverSettings/GyverSettingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GyverSettingsApp.swift
+//  GyverSettings
+//
+//  Created by Vlad V on 22.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct GyverSettingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
